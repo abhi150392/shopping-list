e184558dd5ba7ab130b43e4d81529291ce7a280f
Abhishek Vijayakumar <Abhishekwhizzer@gmail.com>
2023-04-10 23:36:57 -0400

    added form validation

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from 'src/app/Shared/ingredient.model';
 import { ShoppingService } from '../shopping.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,17 +9,12 @@ import { ShoppingService } from '../shopping.service';
   styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
-
-  // @Output() ingredientAdded = new EventEmitter<Ingredient>();
   constructor(private shoppingService: ShoppingService) {}
   ngOnInit(): void {}
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngedient = new Ingredient(ingName, ingAmount);
-    // this.ingredientAdded.emit(newIngedient);
+
+  onAddItem(form: NgForm) {
+    const value = form.value;
+    const newIngedient = new Ingredient(value.name, value.amount);
     this.shoppingService.addIngredient(newIngedient);
   }
 }
