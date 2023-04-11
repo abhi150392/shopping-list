7705c7e5551f6690ce73f417f3ba50a92a89fbfb
Abhishek Vijayakumar <Abhishekwhizzer@gmail.com>
2023-04-11 17:05:05 -0400

    added reactive approach for form validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Header/header.component';
@@ -30,7 +30,7 @@ import { ReceipeEditComponent } from './receipes/receipe-edit/receipe-edit.compo
     ReceipeStartComponent,
     ReceipeEditComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [ShoppingService],
   bootstrap: [AppComponent],
 })
--- src/app/receipes/receipe-edit/receipe-edit.component.ts
+++ src/app/receipes/receipe-edit/receipe-edit.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormArrayName,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { ReceipeService } from '../receipe.service';
 
 @Component({
   selector: 'app-receipe-edit',
@@ -9,11 +17,57 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class ReceipeEditComponent implements OnInit {
   id: number;
   editMode = false;
-  constructor(private route: ActivatedRoute) {}
+  receipeForm: FormGroup;
+  constructor(
+    private route: ActivatedRoute,
+    private receipeService: ReceipeService
+  ) {}
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.editMode = params['id'] != null;
+      this.initForm();
     });
+  }
+
+  private initForm() {
+    let receipeName = '';
+    let receipeImagePath = '';
+    let receipeDescription = '';
+    let receipeIngredients = new FormArray([]);
+    if (this.editMode) {
+      const receipe = this.receipeService.getReceipe(this.id);
+      receipeName = receipe.name;
+      receipeImagePath = receipe.imagePath;
+      receipeDescription = receipe.description;
+      if (receipe['ingredients']) {
+        for (let ingredient of receipe.ingredients) {
+          receipeIngredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name, Validators.required),
+              amount: new FormControl(ingredient.amount, Validators.required),
+            })
+          );
+        }
+      }
+    } else {
+      this.receipeForm = new FormGroup({
+        name: new FormControl(receipeName, Validators.required),
+        imagePath: new FormControl(receipeImagePath, Validators.required),
+        description: new FormControl(receipeDescription, Validators.required),
+        ingredients: receipeIngredients,
+      });
+    }
+  }
+  onSubmit() {
+    console.log(this.receipeForm);
+  }
+
+  onAdd() {
+    (<FormArray>this.receipeForm.get('ingredients')).push(
+      new FormGroup({
+        name: new FormControl(),
+      })
+    );
   }
 }
